ece1388edbf93bef91c89f8cec69f7c741ddeb63
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-06-24 15:09:38 +0700

    Initial Setup Project

---
+++ credit-application/src/main/java/org/ben/creditapplication/CreditApplication.java
@@ -0,0 +1,13 @@
+package org.ben.creditapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditApplication.class, args);
+	}
+
+}
